ee923b3e7da168fe901091d2c34206a35c5e065e
jinglad <57634584+jinglad@users.noreply.github.com>
2020-10-02 19:10:01 +0600

    root url added

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
 
 
+const MongoClient = require('mongodb').MongoClient;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vpsgc.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const app = express();
@@ -17,6 +17,8 @@ const port = 5000;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
     const productsCollection = client.db("emaJohnSimple").collection("products");
+
+    console.log('database connected jin');
 
     app.post('/addProduct', (req, res) => {
         const product = req.body;
@@ -66,6 +68,9 @@ client.connect(err => {
 
 
 // console.log(process.env.DB_USER);
+app.get('/', (req, res) => {
+    res.send('hello jin! welcome back')
+})
 
 
 app.listen(process.env.PORT || port);
